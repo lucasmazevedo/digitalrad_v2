06fa41fbbec9ec5b15482907b2ee280f69b2c494
LUCAS AZEVEDO <lucazmazevedo@gmail.com>
2018-05-17 02:00:28 -0300

    fix assets

--- config/deploy.rb
+++ config/deploy.rb
@@ -26,7 +26,7 @@ set :deploy_to, "/var/www/digitalrad"
 append :linked_files, "config/database.yml", "config/storage.yml", "config/master.key"
 # Default value for linked_dirs is []
 # append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
-append :linked_dirs, "log", "tmp"
+append :linked_dirs, "log", "tmp", "public/sprites"
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
