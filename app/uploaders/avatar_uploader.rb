9334c114e278bd2868d60bcd827073bb467764f2
LUCAS AZEVEDO <lucazmazevedo@gmail.com>
2018-05-20 20:41:32 -0300

    bugs

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -41,6 +41,8 @@ class AvatarUploader < CarrierWave::Uploader::Base
     process :resize_to_fill => [140, 140]
   end
 
+
+
   # Create different versions of your uploaded files:
   # version :thumb do
   #   process resize_to_fit: [50, 50]
